44cadeb1d3abe43793bf8c058c892c600b426e0f
thomas-tank-corp <tharris@hashicorp.com>
2021-04-19 12:11:14 +0100

    org var

--- team.tf
+++ team.tf
@@ -1,6 +1,6 @@
 resource "tfe_team" "team" {
   name         = "team1"
-  organization = "emea-se-playground-2019"
+  organization = var.org
   organization_access {
     manage_policies = false
     manage_workspaces = false
