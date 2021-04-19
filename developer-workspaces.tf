7e2a60de3f1036df8447808b277fca5c7d5825f4
thomas-tank-corp <tharris@hashicorp.com>
2021-04-19 16:57:22 +0100

    fix resource name

--- developer-workspaces.tf
+++ developer-workspaces.tf
@@ -1,10 +1,10 @@
 ///////////////////////////////////////////
-resource "tfe_workspace" "app1" {
+resource "tfe_workspace" "app3" {
   name         = "app1"
   organization = tfe_org.org.id
 }
 
-resource "tfe_team_access" "app1" {
+resource "tfe_team_access" "app3" {
   team_id      = tfe_team.team.id
   workspace_id = tfe_workspace.app1.id
   permissions {
@@ -21,12 +21,12 @@ resource "tfe_team_access" "app1" {
 
 
 //////////////////////////////////////////
-resource "tfe_workspace" "app2" {
+resource "tfe_workspace" "app4" {
   name         = "app2"
   organization = tfe_org.org.id
 }
 
-resource "tfe_team_access" "app2" {
+resource "tfe_team_access" "app4" {
   team_id      = tfe_team.team.id
   workspace_id = tfe_workspace.app2.id
   permissions {
